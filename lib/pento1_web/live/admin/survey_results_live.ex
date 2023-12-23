f5bb247302655959a13d71e7a1ba56c975a3527e
gon <y2gon2@gmail.com>
2023-12-23 12:14:47 +0900

    ch08_2 dashboard Contex 를 사용하여 그래프 넣기
    Contex  library  를 사용하여  앞에서 작업한 상품별 별점 평균 값을 그래프로 그린다.
    
    mix.exs
     - Contex 의존성 추가 후 `mix deps.get`
    
    admin/survey_results_live.ex
     - 그래프를 그리는데 필요한 설정값들을 assign 에 추가
    
      1. SVG chart 생성을 위한 Contex.Dataset 구현 reducer
          - 앞에서 작업한 상품별 별점 평균 (`:product_with_average_ratings`) 에 대한 값을 받아서  `Contex.Dataset` 의 값으로 넣어서 그래프를 그릴 수 있는 `dataset` 을 생성하고, 이를 assign 에 할당한다.
    
      2. bar chart 를 그리기 위한 data 로  변환 reducer
          - `dataset` assgin 을 받아서 `Contex.BarChart` 생성 data 값으로 넣어 주고, 이를 또다시 assign 에 `:chart` key-value를 할당한다.
    
      3. Chart SVG 설정 및 실제 SVG 생성 reducer
          - `:chart` 값을 받아서 , chart 제목, 소제목,  x-축 범례,  y-축 범례,  그래프 크기를 모두 설정하여 SVG 를 생성, 이를 assign `:chart_svg` 값으로 할당하여 전달한다.
    
    admin/survey_results_live.html.heex
     - `survey_results_live.ex` 에서 모든 data 를 생성 및 SVG 를 그려서  assgin 에 할당한 값을  `<%= @chart_svg %>` 로 불러와서 redering 진행.
    
    assets/css/app.css
     - plot 구현에 필요한 css 추가

--- lib/pento1_web/live/admin/survey_results_live.ex
+++ lib/pento1_web/live/admin/survey_results_live.ex
@@ -1,3 +1,81 @@
 defmodule Pento1Web.Admin.SurveyResultsLive do
   use Pento1Web, :live_component
+  alias Pento1.Catalog
+  alias Contex.Plot
+
+  def update(assigns, socket) do
+    {:ok,
+      socket
+      |> assign(assigns)
+      |> assign_products_with_average_ratings()
+      |> assign_dataset()
+      |> assign_chart()  # ex. column_map: %{category_col: 0, value_cols: [1]}
+      |> assign_chart_svg()
+    }
+  end
+
+  # ----------------------------------------------------------------------
+  def assign_products_with_average_ratings(socket) do
+    socket
+    |> assign(
+      :products_with_average_ratings,
+      Catalog.products_with_average_rating()
+    )
+  end
+
+  # ----------------------------------------------------------------------
+  # SVG (Scalable Vector Graphics) Chart 생성을 위한 Contex.Dataset 구현
+  def assign_dataset(%{
+    assigns: %{products_with_average_ratings: products_with_average_ratings}
+    } = socket) do
+    socket
+    |> assign(:dataset, make_bar_chart_dataset(products_with_average_ratings))
+  end
+
+  defp make_bar_chart_dataset(data) do
+    Contex.Dataset.new(data)
+  end
+
+  # ----------------------------------------------------------------------
+  # bar chart 설정값을  socket state 추가 reducer function
+  defp assign_chart(%{assigns: %{dataset: dataset}} = socket) do
+    socket
+    |> assign(:chart, make_bar_chart(dataset))
+  end
+
+  # bar chart 설정
+  defp make_bar_chart(dataset) do
+    Contex.BarChart.new(dataset)
+  end
+
+  # ----------------------------------------------------------------------
+  # plot 설정값 assign state 추가
+  defp assign_chart_svg(%{assigns: %{chart: chart}} = socket) do
+    socket
+    |> assign(:chart_svg, render_bar_chart(chart))
+  end
+
+  # Polt 값 설정
+  defp render_bar_chart(chart) do
+    Plot.new(500, 400, chart)
+    |> Plot.titles(title(), subtitle())
+    |> Plot.axis_labels(x_asis(), y_axis())
+    |> Plot.to_svg()
+  end
+
+  defp title do
+    "Product Ratings"
+  end
+
+  defp subtitle do
+    "average star ratings per product"
+  end
+
+  defp x_asis do
+    "products"
+  end
+
+  defp y_axis do
+    "stars"
+  end
 end
