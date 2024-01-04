a36d24fcb8a7cde95d8325328c5c085b7360537f
gon <y2gon2@gmail.com>
2024-01-04 17:36:32 +0900

    ch13_1 플레이어 플레이 (액션) 제어
    동적 rendering 을 적용하려면,  플레어어가 작동시키는 마우스 동작에 적절하게 반응 하여야 한다.  플레이어가 사용할 동작에 대해 정의하고, 게임에서 필요하고, 예외 사항을 발생시키지 않는 상태임을 확인하는 함수를 구현한다.
    
    game/board.ex
    화면상 게임을 제어하는 모든 동작은 board 위에서 발생하므로, 이를 정의한다.
    
      - `pick/2`  :  클릭 동작에 대한 정의
        선택 동작은 3가지 상태에서 발생할 수 있으므로 이를 각각 정의한다.
        1. 빈 화면 또는 조각이 없는 곳을 click - 아무일도 일어나지 않음
        2. 현재 선택된 (활성화) 조각이 있는 상태에서 화면의 조각을 click
            i.  click 조각이 활성화 조각과 같다면 비활성화 상태로 돌림. (해당 조각을 놓음)
            ii. 다른 click 했다면, 아무일도 일어나지 않음.
        3.  선택된 (활성화) 조각이 없을 때, 화면의 조각을 click
            click 한 조각이 기존에 board 조각 맞춤에 사용된 조각인지 확인한다.
            i. 기존에 사용한 조각이면 이를 활성화 시키고, 대신 기존 조각 리스트에서 제거한다.
            ii. 새로운 조각이면, 이를 활성화 시킨다.
                -> 새로운 조각이면 이를 화면에 중앙에 보여지도록 구현될 예정이므로, 새 조각을 생성하고 중앙에 위치값을 가지도록 하는 `new_pento/2``midpoints/1` 를 구현한다.
    
     -  `drop/1` :   화면에 조각을 위치 시킴 (가져다 놓음)
    
         1. 활성화 조각이 없을 때,  drop 작업을 하면, 아무 작업도 일어나지 않음
         2. 활성화 조각이 있을 때, 해당 위치에 활성화 조각이 배치 되므로 `board.completed_pentos` 리스트에 추가되고, 활성화 속성은 비워지게된다. 다만, 해당 작업이 유효한지에 대한 확인 작업이 필요한데, 이 부분은 미구현되고, 유효성 판단 함수만 아래와 같이 포함됨
    
     - `legal_drop?` : drop 작업이 유효한지 판단
        1. 활성화 조각이 없을 때는 실제로 동작 되는 것이 없으므로 false (?  없어도 크게 상관 없어 보이는데..)
        2. 활성화 조각이 존재하는 경우,
            i.   놓은 조각의 5 point 가 모두 board 범위 내에 존재하여야 함.
            ii.  놓은 조각의 모든 point 가 기존에 board 에 놓은 조각들의 point 와 겹치지 않아야 함.
            -> 위 두 조건이 모두 true 일 때 drop 가능
    
    - `legal_move?/1`:  조각을 옮기는 (drag)  작업의 유효성 판단. 게임에서 정의한 point 범위 내에서만 drag 가능
    
    game/pentomino.ex
     - `overlapping?/2` : 앞의  `legal_dro?/2` 의 유효성 조건 2_ii  에서 새로 놓는 조각과 기존 list 내 조각 이 서로 겹치지 않는지 하나하나 비교하기 위한 함수

--- lib/pento1/game/board.ex
+++ lib/pento1/game/board.ex
@@ -50,4 +50,82 @@ defmodule Pento1.Game.Board do
 
   defp palette(:all), do: [:i, :l, :y, :n, :p, :w, :u, :v, :s, :f, :x, :t]
   defp palette(:small), do: [:u, :v, :p]
+  defp palette(:medium), do: [:t, :y, :l, :p, :n, :v, :u]
+
+  # 1. 아직 조각이 없는 board 칸을 클릭했을 때 -> 반응 X
+  def pick(board, :board), do: board
+
+  # 2. 이미 활성화된 조각을 다시 클릭 할 때 -> 비활성화 & `:active_pento` 값을 nil 로 변경해서
+  #    다른 조각을 활성화 할 수 있도록 함.
+  #    다만 건드린 조각이 현재 활성화 조각이 아닌 경우, 아무런 반응이 일어나지 않아야 함.
+  def pick(%{active_pento: pento}=board, sname) when not is_nil(pento) do
+    if pento.name == sname do
+      %{board | active_pento: nil}
+    else
+      board
+    end
+  end
+
+  # 3. 현재 활성화된 조각이 없으며, 클릭한 조각이 아직 board.complited_pentos 에 있는지 확인
+  #    -> 있다면, 기존에 놓여진 조각 중 선택된 조각을 활성화
+  #    -> 없다면, 클릭한 새 조각을 활성화 조각에 포함 시킴.
+  def pick(board, shape_name) do
+    active =
+      board.complited_pentos
+      |> Enum.find(&(&1.name == shape_name))     # Enum.find의 결과가 nil이 아닐 경우, 해당 결과를 그대로 사용
+      |> Kernel.||(new_pento(board, shape_name)) # Enum.find의 결과가 nil일 경우, new_pento(board, shape_name)를 호출하고 그 결과를 사용
+
+    # 기존 조각이 선택되었다면, 해당 조각을 completed_pentos 에서는 제거해주어야 하므로 이를 filtering 아여 제거시킴.
+    completed = Enum.filter(board.completed_pentos, &(&1.name != shape_name))
+
+    # acitve_pento: / complited_pentos: value 를 새로 설정함.
+    %{board| active_pento: active, completed_pentos: completed}
+  end
+
+  # 새로 선택된 조각 생성
+  defp new_pento(board, shape_name) do
+    Pentomino.new(name: shape_name, location: midpoints(board))
+  end
+
+  # board 화면 중에 신규로 선택된 조각이 오도록 위치값 설정
+  defp midpoints(board) do
+    {xs, ys} = Enum.unzip(board.points)
+    {midpoints(xs), midpoints(ys)}
+  end
+  defp midpoint(i), do: round(Enum.max(i) / 2.0)
+
+  # 마우스 drop ? 기능에 대한 정의 ?
+  # 선택된게 없다면 drop 기능에서 아무 변화가 일어나지 않음
+  # 현재 조각을 선택한 상태였다면, drop 할때 활성화된 조각을 completed_pentos 에 포함시키고
+  # 활성화 속성을 비운다. (nil)
+  # 다만 drop 기능이 적용되는 범위를 설정해 주어야 하는데 "legal drop checking",
+  # 우선 해당 부분은 미구현함.(game boundary layer 가 필요함)
+  def drop(%{active_pento: nil}=board), do: board
+  def drop(%{active_pento: pento}=board) do
+    board
+    |> Map.put(:active_pento, nil)
+    |> Map.put(:completed_pentos, [pento|board.completed_pentos])
+  end
+
+  # drag ? 는 게임 영역 (point) 내에서만 가능함을 확인
+  def legal_move?(%{active_pento: pento, points: points}=_board) do
+    pento.location in points
+  end
+
+  # 현재 활성화 조각이 없다면 화면 어디서든 drop 해도 상관없이 어떤 반응이 일어나지 않음.
+  # 활성화된 조각이 있는 상태에서 drop 이 발생하면,
+  def legal_drop?(%{active_pento: pento}) when is_nil(pento), do: false
+  def legal_drop?(%{active_pento: pento, points: board_points}=board) do
+    #  1. 그 조각의 point 들이 board boundary 내 모두 존재하는지 확인
+    points_on_board =
+      Pentomino.to_shape(pento).points
+      |> Enum.all?(fn point -> point in board_points end)
+
+    #  2. 그 조각의 point 들이 기존의 놓여진 다른 조각과 겹치지 않는지 확인
+    no_overlapping_pentos =
+      !Enum.any?(board.completed_pentos, &Pentomino.overlapping?(pento, &1))
+
+    # 두 조건 모두 true 이어야 drop 가능임을 반환
+    points_on_board and no_overlapping_pentos
+  end
 end
--- lib/pento1/game/pentomino.ex
+++ lib/pento1/game/pentomino.ex
@@ -42,6 +42,14 @@ defmodule Pento1.Game.Pentomino do
 
   # -------------------------------------------------
 
+  # 플레이어가 새로운 조각을 놓았을 때 (drop), 기존에 놓여져 있는 조각과 겹치지 않는지 확인
+  def overlapping?(pento1, pento2) do
+    {p1, p2} = {to_shape(pento1).points, to_shape(pento2).points}
+    Enum.count(p1 -- p2) != 5
+    # "--" 두번째 list 원소가 첫번째 list 원소에 있으면 이를 제거한  첫번째 list
+    # 모든 조각은 5개 point 로 되어있으므로 p1 조각 point 값들이 p2 조각 point 값들과 전혀 겹
+  end
+
   def to_shape(pento) do
     Shape.new(pento.name, pento.rotation, pento.reflected, pento.location)
   end
